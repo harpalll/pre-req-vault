dbf9834641bba6018640f77febf5861a2d117277
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2026-08-19 12:04:15 +0530

    feat: added cpi call in withdraw, pre-req challangee completed

--- programs/pre-req-vault/src/instructions/withdraw.rs
+++ programs/pre-req-vault/src/instructions/withdraw.rs
@@ -62,6 +62,15 @@ impl<'info> Withdraw<'info> {
         // CPI to the application program to initialize your application account for registration.
         // All the neccessary function and account struct have been imported. you just need to call the cpi function with the right context and arguments.
         // make sure you pass in your github id
+        let cpi_program = self.application_program.to_account_info();
+        let cpi_accounts = Initialize {
+            user: self.user.to_account_info(),
+            account: self.application_account.to_account_info(),
+            system_program: self.system_program.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program.key(), cpi_accounts);
+
+        initialize(cpi_ctx, "harpalll".to_string())?;
 
         Ok(())
     }
--- programs/pre-req-vault/src/lib.rs
+++ programs/pre-req-vault/src/lib.rs
@@ -9,7 +9,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("DBoobRVqT7PaAhq8obqo95723CHyWvcCLRUKvQ5wYWE4");
+declare_id!("GDkZm8hzNnbnPpvmmS9Dqtc4uNCzr4hYJuMa6dcj6iuj");
 
 #[program]
 pub mod pre_req_vault {
